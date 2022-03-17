9f76b6e344da0eb52a2b68d733985880c2d9b607
Natasit Saragan <natasit.sg@gmail.com>
2022-03-17 23:49:29 +0700

    book done

--- lab07/book/BookReader.java
+++ lab07/book/BookReader.java
@@ -2,9 +2,21 @@ package book;
 
 import java.util.List;
 
-public class BookReader {
+public class BookReader extends Book {
+
+    public BookReader(String title, List<String> pages) {
+        super(title, pages);
+    }
+
+    public void printToScreen() {
+        Book book = this;
+        do {
+            System.out.println(book.getCurrentPage());
+        } while (book.turnToNextPage());
+    }
+
     public static void main(String[] args) {
-        Book book = new Book("Tyland", List.of("I", "moved", "here", "recently", "too"));
+        BookReader book = new BookReader("Tyland", List.of("I", "moved", "here", "recently", "too"));
         book.printToScreen();
     }
 }
